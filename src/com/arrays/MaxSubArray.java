970be00770b6b29b9437976edb9fa7a089bf1ec1
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-09-06 13:18:15 +0530

    maxArraySumUsingBruteForceAlgo

--- src/com/arrays/MaxSubArray.java
+++ src/com/arrays/MaxSubArray.java
@@ -3,15 +3,15 @@ package com.arrays;
 public class MaxSubArray {
     public static void main(String[] args) {
         int[] nums = {1, -2, 6, -1, 3};
-        maximumSubArraySumUsingBruteForceAlgo(nums);    
+        maximumSubArraySumUsingBruteForceAlgo(nums);
     }
 
-    public static void maximumSubArraySumUsingBruteForceAlgo(int[] nums){
+    public static void maximumSubArraySumUsingBruteForceAlgo(int[] nums) {
         int maxSum = Integer.MIN_VALUE;
         int currSum = 0;
         for (int i = 0; i < nums.length; i++) {
-            currSum = 0;
             for (int j = i; j < nums.length; j++) {
+                currSum = 0;
                 for (int k = i; k <= j; k++) {
                     currSum += nums[k];
                 }
